121ecd3515102fc9b5d0aa134712370f29668b11
srikar58 <srikar.kantamani@gmail.com>
2023-08-31 20:08:14 -0400

    Setting up frontend and base page layout

---
+++ recommendation_frontend/src/components/Home.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Home(): JSX.Element {
+  const navigate = useNavigate();
+  const [searchTerm, setSearch] = useState("");
+
+  const handleSearch = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (searchTerm === "") {
+    } else {
+      navigate("/results/" + searchTerm);
+    }
+  };
+  return (
+    <>
+      <div className="App">
+        <header>
+          <h1>Learn Better</h1>
+          <form className="search-box" onSubmit={handleSearch}>
+            <input
+              type="search"
+              placeholder="What are you looking for?"
+              value={searchTerm}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </form>
+        </header>
+      </div>
+    </>
+  );
+}
